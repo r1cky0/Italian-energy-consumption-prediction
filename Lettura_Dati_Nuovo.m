72b165c5ece0d470ac01ce5c9088f845c1a5057f
lucacolombo04 <48442855+lucacolombo04@users.noreply.github.com>
2019-04-01 15:45:04 +0200

    Add files via upload

---
+++ Lettura_Dati_Nuovo.m
@@ -0,0 +1,86 @@
+clc 
+close all
+clear all
+
+%% LETTURA DATI
+
+tab = readtable('caricoITAday.xlsx', 'Range', 'A2:C732');
+giorni_anni = tab.giorno_anno;
+giorni_settimana= tab.giorno_settimana;
+dati = tab.dati;
+
+%Metodo per risolvere NaN mettendoli = media giorno prima e giorno
+%dopo
+nulli = isnan(dati);
+for i=1:1:size(dati)
+    if nulli(i)==1
+        dati(i)= (dati(i-1) + dati(i+1))/2;
+    end
+end
+
+%DATI PER MODELLO(PRIMO ANNO)
+giorni_anno_modello = giorni_anni(1:365);
+giorni_settimana_modello = giorni_settimana(1:365);
+dati_modello = dati(1:365);
+
+%PLOT DATI
+figure(1)
+plot(giorni_anno_modello, dati_modello);
+title('DATI PRIMO ANNO')
+xlabel("Giorno anno");
+ylabel("Consumo energetico [kw]");
+grid on
+
+%% MODELLI 
+
+%MODELLO PERIODICITA' ANNUALE
+uni = ones(365,1);
+w_annuale = 2*pi/365;
+
+Phi_annuale = [uni cos(w_annuale*giorni_anno_modello) sin(w_annuale*giorni_anno_modello) ... 
+    cos(2*w_annuale*giorni_anno_modello) sin(2*w_annuale*giorni_anno_modello) ... 
+    cos(3*w_annuale*giorni_anno_modello) sin(3*w_annuale*giorni_anno_modello) ...
+    cos(4*w_annuale*giorni_anno_modello) sin(4*w_annuale*giorni_anno_modello)];
+
+ThetaLS_annuale = Phi_annuale\dati_modello;
+
+y_annuale= Phi_annuale * ThetaLS_annuale;
+
+epsilon_annuale = dati_modello - y_annuale;
+
+figure(2)
+title('MODELLO PERIODICITA'' ANNUALE')
+xlabel("Giorno anno");
+ylabel("Consumo energetico [kw]");
+grid on
+hold on
+plot(giorni_anno_modello, dati_modello)
+plot(y_annuale);
+
+%MODELLO PERIODICITA' SETTIMANALE
+w_settimanale = 2*pi/7;
+
+Phi_settimanale = [cos(w_settimanale*giorni_settimana_modello) sin(w_settimanale*giorni_settimana_modello) ...
+    cos(2*w_settimanale*giorni_settimana_modello) sin(2*w_settimanale*giorni_settimana_modello) ...
+    cos(3*w_settimanale*giorni_settimana_modello) sin(3*w_settimanale*giorni_settimana_modello) ...
+    cos(4*w_settimanale*giorni_settimana_modello) sin(4*w_settimanale*giorni_settimana_modello)];
+
+ThetaLS_settimanale = Phi_settimanale\dati_modello;
+
+y_settimanale= Phi_settimanale * ThetaLS_settimanale;
+
+epsilon_settimanale = dati_modello - y_settimanale;
+
+y_fin = y_annuale + y_settimanale;
+
+figure(3)
+title('MODELLO PERIODICITA'' SETTIMANALE')
+xlabel("Giorno anno");
+ylabel("Consumo energetico [kw]");
+grid on
+hold on
+plot(giorni_anno_modello, dati_modello)
+plot(y_fin);
+
+
+
